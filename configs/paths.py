a67f48221dcac4ec748efb1e7e4d7d64aff2b8e6
Md Badsha Biswas <40556341+badshabiswas@users.noreply.github.com>
2025-04-21 04:25:17 -0400

    Create paths.py

---
+++ configs/paths.py
@@ -0,0 +1,8 @@
+# paths.py
+TRAIN_FILE = "./data/train.tsv"
+TEST_FILE = "./data/val.tsv"
+
+MODEL_PATH = "meta-llama/Llama-3.1-8B-Instruct"
+CACHE_DIR = "/projects/ouzuner/mbiswas2/Llama/Model"
+OUTPUT_MODEL_DIR = "/projects/ouzuner/mbiswas2/FinetunedModel/Llama_OverSample"
+PREDICTION_OUTPUT = "./predictions/predictions_Llama_8B_Oversample.tsv"
